6750f5c95d18eec45d52272a442c6583291c590f
Joseph Dunn <joseph@tx-0.org>
2014-03-25 12:52:39 -0500

    Re-commit builds since merged changes

--- Smart-Table.debug.js
+++ Smart-Table.debug.js
@@ -349,13 +349,38 @@
                         scope.value = getter(scope.row);
 			// Example of how to change type of input field based on UI units.
 			if (scope.value.ui_units_type == 'dollars') {
-			    input[0].type='text';
+			    //input[0].type='text';
 			}
+			input[0].min = scope.value.ui_units_min_val;
+			input[0].max = scope.value.ui_units_max_val;
+			input[0].step = 'any';
                     }, true);
 
                     scope.submit = function () {
-                        //update model if valid
-                        if (scope.myForm.$valid === true) {
+			// We have to validate some contraints manually due to AngularJS limitations
+			// (e.g. min/max ignored if set dynamically) and fact that input-type directive
+			// also causes some type-specific attributes to be ignored.
+
+			if (scope.edit_value < scope.value.ui_units_min_val)
+			{
+			    scope.myForm.myInput.$valid = false;
+			    scope.myForm.myInput.$invalid = true;
+			    scope.myForm.myInput.$error.min = true;
+			    scope.validationErr = "Minimum value is: " + scope.value.ui_units_min_val;
+			}
+			else if (scope.edit_value > scope.value.ui_units_max_val) {
+			    scope.myForm.myInput.$valid = false;
+			    scope.myForm.myInput.$invalid = true;
+			    scope.myForm.myInput.$error.max = true;
+			    scope.validationErr = "Maximum value is " + scope.value.ui_units_max_val;
+			}
+			else if (scope.myForm.myInput.$error.required) {
+			    // Though this code block can be triggered, the new value of scope.validationErr
+			    // may not show in the UI (?).
+			    scope.validationErr = "A value is is required.";
+			}
+                        if (scope.myForm.myInput.$valid === true) {
+			    //console.log("Input value considered valid");
 			    // Since value is an object, we must clone it so that differences between old and new value
 			    // are seen.
 			    var new_value = angular.copy(scope.value);
@@ -364,6 +389,9 @@
                             ctrl.updateDataRow(scope.row, scope.column.map, new_value);
                             ctrl.sortBy();//it will trigger the refresh...  (ie it will sort, filter, etc with the new value)
                         }
+			else {
+			    //console.log("scope.myform.myInput.$valid is false");
+			}
                         scope.toggleEditMode();
                     };
 
@@ -373,6 +401,15 @@
 			// so assign to a different scope variable.
 			scope.edit_value = scope.value.scaled_estimate;
                         scope.isEditMode = scope.isEditMode !== true;
+			if (scope.isEditMode == true) {
+			    scope.validationErr = '';
+			    scope.myForm.myInput.$dirty = false;
+			    scope.myForm.myInput.$valid = true;
+			    scope.myForm.myInput.$invalid = false;
+			    scope.myForm.myInput.$error.min = false;
+			    scope.myForm.myInput.$error.max = false;
+			    scope.myForm.myInput.$error.required = false;
+			}
                     };
 
                     scope.$watch('isEditMode', function (newValue) {
@@ -715,8 +752,9 @@ angular.module("partials/editableEstimate.html", []).run(["$templateCache", func
     "    <span ng-hide=\"isEditMode\">{{value | format:column.formatFunction:column.formatParameter}}</span>\n" +
     "\n" +
     "    <form ng-submit=\"submit()\" ng-show=\"isEditMode\" name=\"myForm\">\n" +
-    "        <input name=\"myInput\" ng-model=\"edit_value\" type=\"type\" input-type/>\n" +
+    "        <input name=\"myInput\" ng-model=\"edit_value\" type=\"type\" required input-type />\n" +
     "    </form>\n" +
+    "    <p ng-show=\"!myForm.myInput.$valid && !myForm.myInput.$pristine\" class=\"help-block\">{{validationErr}}</p>\n" +
     "</div>\n" +
     "");
 }]);
